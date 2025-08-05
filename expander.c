492b97c1f602a71725c8f63fb9c3b726ae0cc29d
normcrusher42 <nanasser@student.42abudhabi.ae>
2025-08-06 03:25:13 +0400

    god damn I can't continue this now but still almost done just realized norminettes a bitch

--- expander.c
+++ expander.c
@@ -1,8 +1,38 @@
 #include "minishell.h"
 
-char	*env_expander
+char	*merge_strings(char *token, char *frst_str, char *thrd_str)
+{
+	
+}
 
-char	*var_expander(char *token, int last_status)
+char	*env_expander(char *token, char *frst_str, char *thrd_str, char **envp)
+{
+	char	*updated_token;
+	char	*var_name;
+	char	*value;
+	int		i;
+	int		start;
+
+	i = -1;
+	while (token[++i])
+	{
+		if (token[i] == '$' && (ft_isalnum(token[i + 1]) || token[i + 1] == '_'))
+		{
+			start = i + 1;
+			while (ft_isalnum(token[start]) || token[start] == '_')
+				start++;
+			frst_str = ft_substr(token, 0, i);
+			var_name = ft_substr(token, i + 1, start - (i + 1));
+			thrd_str = ft_substr(token, start, ft_strlen(token));
+			value = get_env_value(envp, var_name);
+			if (!value)
+				value = NULL;
+			updated_token = ft_strjoin3(frst_str, value, thrd_str);
+		}
+	}
+}
+
+char	*var_expander(char *token, int last_status, char **envp)
 {
 	int		i;
 	char	*new_token;
@@ -14,8 +44,6 @@ char	*var_expander(char *token, int last_status)
 	status_str = ft_itoa(last_status);
 	if (!status_str)
 		return (NULL);
-	if (!new_token)
-		return (free(status_str), NULL);
 	while (token[++i])
 	{
 		if (token[i] == '$' && token[i + 1] == '?')
@@ -25,9 +53,7 @@ char	*var_expander(char *token, int last_status)
 			new_token = ft_strjoin3(first_str, status_str, third_str);
 		}
 		else
-		{
-			
-		}
+			new_token = env_expander()
 	}
 	return (free(status_str), free(first_str), free(third_str), new_token);
 }
@@ -46,7 +72,7 @@ char	**expand_token(t_token *token, char **envp, int last_status)
 		if (ft_strchr(token->tokens[i], '$') && token->quote[i] != QTE_SINGLE)
 			result[i] = var_expander(token->tokens[i], last_status);
 		else
-			result[i] = token->tokens[i];
+			result[i] = ft_strdup(token->tokens[i]);
 		if (!result[i])
 			return (NULL);
 	}
