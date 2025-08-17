8b35bde7a14fafdef7d120462f250ae929d5ad41
normcrusher42 <nanasser@student.42abudhabi.ae>
2025-08-17 20:03:50 +0400

    added the cd builtin, got 2-3 more to go

--- builtins.c
+++ builtins.c
@@ -34,6 +34,33 @@ int	is_builtin(char *cmd)
 	return (0);
 }
 
+// A simple remake of 'cd'. Changes current working directory based on input
+int	ft_cd(char **av, char **envp)
+{
+	char	*path;
+	char	*old_path;
+	char	cwd[1024];
+
+	if (ft_arrlen(av) > 2)
+		return (perror("-mOdy: cd: too many arguments"), 1);
+	else if (ft_arrlen(av) == 1)
+	{
+		path = get_env_value(envp, "HOME");
+		if (!path)
+			return (perror("-mOdy: cd: HOME not set"), 1);
+	}
+	else
+		path = av[1];
+	old_path = get_env_value(envp, "PWD");
+	if (chdir(path) != 0)
+		return (perror("-mOdy: cd: path error"), 1);
+	if (old_path)
+		set_env_value(&envp, "OLDPWD", old_path);
+	if (getcwd(cwd, sizeof(cwd)))
+		set_env_value(&envp, "PWD", cwd);
+	return (0);
+}
+
 // A simple remake of 'env'. Prints all environment variables
 int	ft_env(char **envp)
 {
@@ -48,7 +75,7 @@ int	ft_env(char **envp)
 	return (0);
 }
 
-// A simple remake of 'pwd'
+// A simple remake of 'pwd', prints the current working directory
 int	ft_pwd(void)
 {
 	char	buf[1024];
@@ -62,7 +89,7 @@ int	ft_pwd(void)
 	return (1);
 }
 
-// A simple remake of 'echo' with the -n flag included.
+// A simple remake of 'echo' with the -n flag included. Prints input arguments
 int	ft_echo(char **av)
 {
 	int	i;
